1cbde17261fba1a2bcf0f5b5224730fe19cf6a6d
DevelopWithAP <apostolisparga@gmail.com>
2022-12-28 14:03:07 +0200

    Deleted Procfile && prepared for deployment on Netlify

--- build/src/routes/index.js
+++ build/src/routes/index.js
@@ -4,14 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const images_1 = __importDefault(require("./api/images"));
 const routes = express_1.default.Router();
 routes.use('/api/images', images_1.default);
 routes.get('/', (req, res) => {
-    res.send(`<h3>Main API route</h3>
-     <hr> 
-    <p> Convert your image by specifying filename-with exptension, width and height as a URL parameters</p>
-    <p> Example: locahost:3000/api/images?filename=my_filename.jpg&width=my_width&height=my_height </p>
-     `);
+    res.sendFile(path_1.default.join(__dirname, '/index.html'));
 });
 exports.default = routes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,12 @@
 import express from 'express';
+import path from 'path';
 import images from './api/images';
 
 const routes: express.Router = express.Router();
 
 routes.use('/api/images', images);
 routes.get('/', (req: express.Request, res: express.Response): void => {
-  res.send(`<h3>Main API route</h3>
-     <hr> 
-    <p> Convert your image by specifying filename-with exptension, width and height as a URL parameters</p>
-    <p> Example: locahost:3000/api/images?filename=my_filename.jpg&width=my_width&height=my_height </p>
-     `);
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 export default routes;
